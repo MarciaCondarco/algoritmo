282cb49a3719444c042e2eae9f0a6d7464adce7e
MarciaCondarco <142843705+MarciaCondarco@users.noreply.github.com>
2024-05-23 11:57:12 -0300

    aula sobre operadores lógicos

---
+++ atividadetabua.js
@@ -0,0 +1,23 @@
+var numeroEscolhido = 8
+
+var num1 = numeroEscolhido * 1
+var num2 = numeroEscolhido * 2
+var num3 = numeroEscolhido * 3
+var num4 = numeroEscolhido * 4
+var num5 = numeroEscolhido * 5
+var num6 = numeroEscolhido * 6
+var num7 = numeroEscolhido * 7
+var num8 = numeroEscolhido * 8
+var num9 = numeroEscolhido * 9
+var num10 = numeroEscolhido * 10
+
+console.log(numeroEscolhido, "x 1 = ", num1)
+console.log(numeroEscolhido, "x 2 = ", num2)
+console.log(numeroEscolhido, "x 3 = ", num3)
+console.log(numeroEscolhido, "x 4 = ", num4)
+console.log(numeroEscolhido, "x 5 = ", num5)
+console.log(numeroEscolhido, "x 6 = ", num6)
+console.log(numeroEscolhido, "x 7 = ", num7)
+console.log(numeroEscolhido, "x 8 = ", num8)
+console.log(numeroEscolhido, "x 9 = ", num9)
+console.log(numeroEscolhido, "x 10 = ", num10)
---
+++ operadoreslogicos.js
@@ -0,0 +1,21 @@
+/*
+atribuição -> =
+soma -> +
+subtração-> -
+divisão-> /
+multiplicação-> *
+------------------------------
+valores operador valor
+3          -       3
+------------------------------
+igual =>   ==
+------------------------------
+3 == 3 -> true
+
+diferente ->                 !=  |   3 != 3 | false            | 3 != 3.1 | true 
+maior   ->        >
+menor   ->        <          | 4 < 3 | true
+maior igual ->      >=       |4 >= 4 | true
+menor igual ->      <=       |5 <= 5 | true
+negação       ->            !    | !false 
+*/
